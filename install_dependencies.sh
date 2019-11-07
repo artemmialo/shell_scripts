7774038c9b2b4f629b06f905b93f77c2588074b5
Artem Myalo <46872267+artemmialo@users.noreply.github.com>
2019-11-07 16:24:35 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e
 
 #Install Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 #Install Terraform
-sudo apt-get install wget unzip --yes
+apt-get install wget unzip --yes
 wget https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip
 unzip terraform_0.12.13_linux_amd64.zip
-sudo mv terraform /usr/local/bin
+mv terraform /usr/local/bin
 
 #Install nodejs
-sudo snap install node --classic --channel=13
+snap install node --classic --channel=13
 
 #Register Microsoft key and feed
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 
 #Install the .NET 3.0 SDK
-sudo add-apt-repository universe
-sudo apt-get update --yes
-sudo apt-get install apt-transport-https --yes
-sudo apt-get update --yes
-sudo apt-get install dotnet-sdk-3.0 --yes
+add-apt-repository universe
+apt-get update --yes
+apt-get install apt-transport-https --yes
+apt-get update --yes
+apt-get install dotnet-sdk-3.0 --yes
