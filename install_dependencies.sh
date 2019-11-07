79c8c159f5d3160ccb08501907397f8a69ce6790
Artem Myalo <46872267+artemmialo@users.noreply.github.com>
2019-11-07 15:27:30 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -22,11 +22,3 @@ sudo apt-get update --yes
 sudo apt-get install apt-transport-https --yes
 sudo apt-get update --yes
 sudo apt-get install dotnet-sdk-3.0 --yes
-
-#Add VM to Azure DevOps
-sudo mkdir myagent && cd myagent
-wget https://vstsagentpackage.azureedge.net/agent/2.160.0/vsts-agent-linux-x64-2.160.0.tar.gz
-tar zxvf vsts-agent-linux-x64-2.160.0.tar.gz
-sudo ./config.sh --url https://dev.azure.com/foodtruckinc/ --auth pat --token $1 --unattended --pool $2 --agent $3 --acceptTeeEula OB
-sudo ./svc.sh install
-sudo ./svc.sh start
